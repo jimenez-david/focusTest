d7b6e81d8f39cf872790ebb52f772fa5621f694d
jose.jimenez <jose.jimenez@HP-RW368.rulesware.local>
2019-10-25 08:06:30 -0600

    Adding code to click on link

--- test/src/test/java/test/FocusAutoTest.java
+++ test/src/test/java/test/FocusAutoTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FocusAutoTest {
@@ -32,7 +33,12 @@ public class FocusAutoTest {
 			WebElement inputButton = driver.findElement(By.name("btnK"));
 			wait.until(ExpectedConditions.elementToBeClickable(inputButton));				
 			inputButton.click();
-						
+			
+			/*Clicking on Focus link*/
+			WebElement focusLink = driver.findElement(By.xpath("//a[contains (@href, 'https://www.focusservices.com/')]"));
+			Assert.assertTrue(focusLink.isDisplayed(), "Link is displayed");		
+			focusLink.click();
+			
 			/*Killing driver session*/
 			driver.quit();
 		}
